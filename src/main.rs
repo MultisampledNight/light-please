19458e1c190f1f945dbdb35a1dd57c4565f832af
MultisampledNight <contact@multisamplednight.com>
2024-05-06 16:48:42 +0200

    feat: add strobe mode

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::num::NonZeroU32;
+use std::{num::NonZeroU32, time::{Duration, Instant}};
 
+use clap::Parser;
 use eyre::{format_err, Result, WrapErr};
 use winit::{
     dpi::PhysicalSize,
@@ -8,14 +9,33 @@ use winit::{
     window::Window,
 };
 
+#[derive(Parser)]
+struct Args {
+    #[clap(long = "strobe", default_value_t = false)]
+    strobe: bool,
+}
+
+#[derive(Default)]
+struct Strobe {
+    on: bool,
+}
+
+impl Strobe {
+    fn toggle(&mut self) {
+        self.on = !self.on;
+    }
+}
+
 struct State<'win> {
     surface: softbuffer::Surface<&'win Window, &'win Window>,
     _context: softbuffer::Context<&'win Window>,
     window: &'win Window,
+
+    strobe: Option<Strobe>,
 }
 
 impl<'win> State<'win> {
-    pub fn new(window: &'win Window) -> Result<Self> {
+    pub fn new(window: &'win Window, args: Args) -> Result<Self> {
         let context = softbuffer::Context::new(window)
             .map_err(|e| format_err!("while creating softbuffer context: {e}"))?;
         let surface = softbuffer::Surface::new(&context, window)
@@ -25,6 +45,7 @@ impl<'win> State<'win> {
             surface,
             _context: context,
             window,
+            strobe: args.strobe.then(Strobe::default)
         };
 
         // initial resize needed for the surface to configure itself
@@ -35,14 +56,28 @@ impl<'win> State<'win> {
     }
 
     pub fn process(&mut self, elwt: &EventLoopWindowTarget<()>, event: Event<()>) -> Result<()> {
-        match event {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::RedrawRequested => self.draw()?,
+        let flow = if self.strobe.is_some() { 
+            self.draw()?;
+
+            let now = Instant::now();
+            let fps = 20;
+            let next_frame = now + Duration::from_millis(1000 / fps);
+
+            ControlFlow::WaitUntil(next_frame)
+        } else {
+            ControlFlow::Wait
+        };
+
+        dbg!(flow);
+
+        elwt.set_control_flow(flow);
+        if let Event::WindowEvent { event, .. } = event {
+            match event {
+                WindowEvent::RedrawRequested if self.strobe.is_none() => self.draw()?,
                 WindowEvent::Resized(new_size) => self.resize(new_size)?,
                 WindowEvent::CloseRequested => elwt.exit(),
                 _ => (),
-            },
-            _ => (),
+            }
         }
 
         Ok(())
@@ -68,8 +103,17 @@ impl<'win> State<'win> {
             .buffer_mut()
             .map_err(|e| format_err!("could not get reference to display buffer: {e}"))?;
 
-        let light_color = (255 << 16) | (255 << 8) | 255;
-        buffer.fill(light_color);
+        let off_color = 0;
+        let on_color = (255 << 16) | (255 << 8) | 255;
+
+        buffer.fill(on_color);
+
+        if let Some(ref mut strobe) = self.strobe.as_mut() {
+            if !dbg!(strobe.on) {
+                buffer.fill(off_color);
+            }
+            strobe.toggle();
+        }
 
         buffer
             .present()
@@ -78,13 +122,13 @@ impl<'win> State<'win> {
 }
 
 fn main() -> Result<()> {
+    let args = Args::parse();
     let event_loop = EventLoop::new().wrap_err("while creating event loop")?;
     let window = Window::new(&event_loop).wrap_err("while creating window")?;
-    let mut state = State::new(&window).wrap_err("while creating state")?;
+    let mut state = State::new(&window, args).wrap_err("while creating state")?;
 
     event_loop
         .run(move |event, elwt| {
-            elwt.set_control_flow(ControlFlow::Wait);
             match state.process(elwt, event) {
                 Ok(flow) => flow,
                 Err(e) => {
